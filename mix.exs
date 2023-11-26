f9daa915cafc92a253f03fc4545774af7401d634
Burgy Benjamin <benjamin@roomz.io>
2023-11-26 08:34:49 +0100

    Make the text color inverted to the background.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ElixirGsaTvDashboard.MixProject do
   def project do
     [
       app: :elixir_gsa_tv_dashboard,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
