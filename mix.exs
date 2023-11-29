5ccd171965e8f48bb0cbbdba9de2b15df171accf
Burgy Benjamin <benjamin@roomz.io>
2023-11-29 11:58:35 +0100

    Highlight the current day of week.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ElixirGsaTvDashboard.MixProject do
   def project do
     [
       app: :elixir_gsa_tv_dashboard,
-      version: "0.3.1",
+      version: "0.3.2",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
